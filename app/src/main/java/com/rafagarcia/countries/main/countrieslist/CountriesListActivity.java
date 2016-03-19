825b9c6ce4150a274636fbc9506517ac19e716ea
Rafa Garcia <rafagarfer@gmail.com>
2016-03-19 11:04:21 +0000

    Setting up collapsing tool bar layout...

--- app/src/main/java/com/rafagarcia/countries/main/countrieslist/CountriesListActivity.java
+++ app/src/main/java/com/rafagarcia/countries/main/countrieslist/CountriesListActivity.java
@@ -6,23 +6,38 @@ import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.rafagarcia.countries.MyApplication;
 import com.rafagarcia.countries.R;
 import com.rafagarcia.countries.main.country.CountryActivity;
-import com.rafagarcia.countries.model.Country;
 
 public class CountriesListActivity extends AppCompatActivity implements CountriesListFragment.OnFragmentInteractionListener {
 
     public static final String COUNTRY = "country";
-
+    private Toolbar mToolBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportFragmentManager().beginTransaction()
                 .add(R.id.fragment, CountriesListFragment.newInstance()).commit();
+
+        setToolbar();
+    }
+
+    private void setToolbar() {
+        mToolBar = (Toolbar) findViewById(R.id.toolBar);
+        if (mToolBar != null) {
+            mToolBar.setTitle(R.string.countries_title);
+            setSupportActionBar(mToolBar);
+            android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+            if (actionBar != null) {
+                actionBar.setDisplayHomeAsUpEnabled(true);
+                actionBar.setDisplayShowHomeEnabled(true);
+            }
+        }
     }
 
     @Override
--- app/src/main/java/com/rafagarcia/countries/main/country/CountryActivity.java
+++ app/src/main/java/com/rafagarcia/countries/main/country/CountryActivity.java
@@ -2,8 +2,10 @@ package com.rafagarcia.countries.main.country;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.widget.FrameLayout;
 
@@ -14,6 +16,8 @@ import com.rafagarcia.countries.model.Country;
 
 public class CountryActivity extends AppCompatActivity implements CountryFragment.OnFragmentInteractionListener{
 
+    private Toolbar mToolBar;
+    private CollapsingToolbarLayout mCollapsingToolbarLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +30,22 @@ public class CountryActivity extends AppCompatActivity implements CountryFragmen
         }
         catch (Exception e){
             Log.e("Error", e.getMessage());
+        }
+
+        setToolbar();
+    }
+
+    private void setToolbar() {
+        mToolBar = (Toolbar) findViewById(R.id.toolBar);
+        mCollapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsingToolbarLayout);
+        if (mToolBar != null) {
+            mCollapsingToolbarLayout.setTitle(getResources().getString(R.string.countries_title));
+            setSupportActionBar(mToolBar);
+            android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+            if (actionBar != null) {
+                actionBar.setDisplayHomeAsUpEnabled(true);
+                actionBar.setDisplayShowHomeEnabled(true);
+            }
         }
     }
 
