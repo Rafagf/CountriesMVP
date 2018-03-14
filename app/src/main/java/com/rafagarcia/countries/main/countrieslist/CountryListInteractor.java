489f7bb3c767637e2d1564acd511893a4fc84b86
Rafagf <rafagarfer@gmail.com>
2018-03-14 16:54:40 +0700

    Formatting

--- app/src/main/java/com/rafagarcia/countries/main/countrieslist/CountryListInteractor.java
+++ app/src/main/java/com/rafagarcia/countries/main/countrieslist/CountryListInteractor.java
@@ -28,7 +28,6 @@ public class CountryListInteractor implements CountryListMvp.Interactor {
 
     @Override
     public Maybe<List<Country>> getCountries() {
-
         Maybe<List<Country>> memorySource = memoryDataSource.getCountries();
         Maybe<List<Country>> localSource = localDataSource.getCountries().doOnSuccess(countries ->
                 memoryDataSource.save(countries));
